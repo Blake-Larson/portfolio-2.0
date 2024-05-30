c19e2ba9ce698236705c3be055a36cb4a39d9daa
Bake Larson <blake.larson@aofl.com>
2024-05-29 18:29:22 -0700

    Header component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
-<h1 class="text-3xl font-bold underline">
-	Hello world!
-</h1>
-  
+<script>
+	import Header from './Header.svelte'; // Importing the Button component
+</script>
+
+<body
+	class="bg-slate-900 leading-relaxed text-slate-400 antialiased selection:bg-teal-300 selection:text-teal-900"
+>
+	<Header />
+</body>
+
 <style lang="postcss">
-:global(html) {
-	background-color: theme(colors.gray.100);
-}
+	:global(html) {
+		background-color: theme(colors.slate.900);
+	}
 </style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<header>
+	<h1 class="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Blake Larson</h1>
+	<h2 class="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+		Software Engineer
+	</h2>
+	<p class="mt-4 max-w-xs leading-normal">
+		I build pixel-perfect, engaging, and accessible digital experiences.
+	</p>
+</header>
+
+<style>
+</style>
